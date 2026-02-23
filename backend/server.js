3cc2691a38ea60d968a020570588c8a7b46d5034
gumjim03-stack <gumjim03@gmail.com>
2026-02-22 18:25:31 -0800

    fix duplicate mongoose declaration

--- backend/server.js
+++ backend/server.js
@@ -29,8 +29,6 @@ const upload = multer({ storage });
 // 4️⃣ DESPUÉS YA CREAS APP
 const app = express();
 
-const mongoose = require('mongoose');
-
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB conectado"))
     .catch(err => console.error("Error MongoDB:", err));
